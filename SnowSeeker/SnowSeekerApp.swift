f247588ed9565d128ef99252ea7e536fb2edc78b
Anna Filin <68apple@gmail.com>
2024-11-11 13:34:21 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Anna Filin on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
